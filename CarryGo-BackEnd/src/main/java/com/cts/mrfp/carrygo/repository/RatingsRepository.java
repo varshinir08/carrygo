8b34fb2c90ad41d37206008646032a3902d8be75
Kurlem <2479349@cognizant.com>
2026-03-31 14:11:14 +0530

    completed ratingscontroller

--- CarryGo-BackEnd/src/main/java/com/cts/mrfp/carrygo/repository/RatingsRepository.java
+++ CarryGo-BackEnd/src/main/java/com/cts/mrfp/carrygo/repository/RatingsRepository.java
@@ -1,7 +1,8 @@
 package com.cts.mrfp.carrygo.repository;
-
 import com.cts.mrfp.carrygo.model.Ratings;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface RatingsRepository extends JpaRepository<Ratings,Integer> {
+public interface RatingsRepository extends JpaRepository<Ratings, Integer> {
+    List<Ratings> findByCommuterUserId(Integer commuterId);
 }
--- CarryGo-BackEnd/src/main/java/com/cts/mrfp/carrygo/service/RatingsService.java
+++ CarryGo-BackEnd/src/main/java/com/cts/mrfp/carrygo/service/RatingsService.java
@@ -1,4 +1,25 @@
 package com.cts.mrfp.carrygo.service;
 
+import com.cts.mrfp.carrygo.model.Ratings;
+import com.cts.mrfp.carrygo.repository.RatingsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
 public class RatingsService {
+
+    @Autowired
+    private RatingsRepository ratingsRepository;
+
+    public Ratings addRating(Ratings rating) {
+        rating.setCreatedAt(LocalDateTime.now());
+        return ratingsRepository.save(rating);
+    }
+
+    public List<Ratings> getRatingsByCommuter(Integer commuterId) {
+        return ratingsRepository.findByCommuterUserId(commuterId);
+    }
 }
