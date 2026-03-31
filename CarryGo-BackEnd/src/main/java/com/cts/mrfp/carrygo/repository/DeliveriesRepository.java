697ea2474da6d2477d79bfee7f2a0dbb22a48626
Kovela <2479597@cognizant.com>
2026-03-31 13:59:44 +0530

    done delivery and wallet controllers

--- CarryGo-BackEnd/src/main/java/com/cts/mrfp/carrygo/repository/DeliveriesRepository.java
+++ CarryGo-BackEnd/src/main/java/com/cts/mrfp/carrygo/repository/DeliveriesRepository.java
@@ -1,10 +1,10 @@
 package com.cts.mrfp.carrygo.repository;
-
 import com.cts.mrfp.carrygo.model.Deliveries;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface DeliveriesRepository extends JpaRepository<Deliveries, Integer> {
     List<Deliveries> findBySenderUserId(Integer userId);
+    List<Deliveries> findByCommuterUserId(Integer userId);
+    List<Deliveries> findByStatus(String status);
 }
--- CarryGo-BackEnd/src/main/java/com/cts/mrfp/carrygo/repository/WalletsRepository.java
+++ CarryGo-BackEnd/src/main/java/com/cts/mrfp/carrygo/repository/WalletsRepository.java
@@ -1,12 +1,8 @@
 package com.cts.mrfp.carrygo.repository;
-
 import com.cts.mrfp.carrygo.model.Wallets;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-@Repository
 public interface WalletsRepository extends JpaRepository<Wallets, Integer> {
-    // Find wallet by the user ID nested in the Wallets entity
     Optional<Wallets> findByUserUserId(Integer userId);
 }
