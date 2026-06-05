6df4349cc708ba0d400c196e7c726cd16f048b25
Varshini <2479800@cognizant.com>
2026-06-05 08:49:07 +0530

    Final CarryGo code

--- CarryGo-BackEnd/src/main/java/com/cts/mrfp/carrygo/repository/ServiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cts.mrfp.carrygo.repository;
-
-import com.cts.mrfp.carrygo.model.Services;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRepository extends JpaRepository<Services, Integer> {
-
-}
